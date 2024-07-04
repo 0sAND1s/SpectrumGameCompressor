c2acb03189b18ca07f46d101523cd2b66abafef4
George CHIRTOACA <george.chirtoaca@gmail.com>
2024-07-04 14:20:05 +0300

    Miminize Venturama loader, to work with HC, due to low ramtop of game.

--- custom-build/Venturama.bat
+++ custom-build/Venturama.bat
@@ -3,5 +3,5 @@ set input=..\input\%name%.tzx
 set output=..\output\%name%.tap
 
 ..\tools\hcdisk2 open %input% : get VENTURAM5 -n %name%.scr : get VENTURAM6 -n %name%.main : exit
-..\tools\hcdisk2 format %output% -y : open %output% : basimp %name%.bas : put %name%.scr -t b -s 16384 -n "venturamas" : put %name%.main -t b -s 24465 -n "venturamac" : dir : exit
+..\tools\hcdisk2 format %output% -y : open %output% : basimp %name%.bas : put %name%.scr -t b -s 16384 -n "ventus" : put %name%.main -t b -s 24465 -n "ventuc" : dir : exit
 del /Q %name%.scr %name%.main
