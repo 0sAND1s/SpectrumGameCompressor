a83e61539979b6c436f607688a1be1d7410002c4
George CHIRTOACA <george.chirtoaca@gmail.com>
2024-09-20 18:57:40 +0300

    Updated Renegade1 to speed it up and include screen.

--- srctap/Renegade1.asm
+++ srctap/Renegade1.asm
@@ -3,7 +3,7 @@
 	DEVICE ZXSPECTRUM48
 	
 game_stack		EQU	19000
-temp_stack		EQU $5C00 - 10
+temp_stack		EQU	$5C00 - 10
 game_start		EQU	$5C00
 game_len		EQU	41888
 game_end		EQU	65439
@@ -19,38 +19,64 @@ StartMobile:
 
 	;move loader into place		
 	di
-	ld		sp, temp_stack 
+	ld	sp, temp_stack 
 	
-	ld		de, StartFixed	
-	ld		hl, StartFixed - StartMobile
-	add		hl, bc						
-	ld		bc, End - StartFixed			
+	ld	de, StartFixed	
+	ld	hl, StartFixed - StartMobile
+	add	hl, bc						
+	ld	bc, End - StartFixed			
 	ldir					
+	
+	;unpack screen
+	IFDEF HAVE_SCR
+	ld	bc, SCR_SIZE + MAIN_SIZE - 1
+	add	hl, bc
+	ld	de, $FFFF	
+	call	Unpack		
+	push	hl	
+		ex	de, hl
+		inc	hl
+	
+		call	ScrDraw
+	pop	hl	
+	ENDIF
 	
 	jp	StartFixed
 
-StartFixed:					
+StartFixed:	
+	ld		bc, -(MAIN_SIZE - 1)
+	add		hl, bc
 	;move game code up in RAM, from begining	
 	ld		bc, MAIN_SIZE
-	;allow safety offset
-	ld		de, game_start - 2
+	;allow safety offset of 4
+	ld		de, game_start - 4	
 	ldir
 				
 	;go back one byte after LDIR
-	dec		de
+	dec	de
 	;unpack game into place
-	ex		de, hl	
-	ld		de, game_end		
+	ex	de, hl	
+	ld	de, game_end		
 	call	Unpack	
 	
-	xor		a
-	out		($fe), a	
-
+	xor	a
+	out	($fe), a	
+	
+	;patch for eliminating slowness due to IN $FF; taken from the version from Rares Atodiresei. blue border still produced.
+	ld	hl, $9B40
+	xor	a
+	ld	(hl), a
+	inc	hl
+	ld	(hl), a
+	
+	
+	
 	include "poker.asm"	
 	
-	ld		sp, game_stack
-	jp		game_entry	
-		
+	ld	sp, game_stack
+	jp	game_entry	
+	
+	include "scr_draw.asm"			
 Unpack:	
 	include	"dzx0_turbo_back.asm"
 End:
